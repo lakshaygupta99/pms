bf1fbd28935a9f58b5e5c39192ec47bbace3e0e8
Lakshay Gupta <lakshay.gupta@timesinternet.in>
2021-08-05 12:50:44 +0530

    Added menu

--- src/com/hibernate/Main.java
+++ src/com/hibernate/Main.java
@@ -1,10 +1,36 @@
 package com.hibernate;
 
+import java.util.Scanner;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
 public class Main {
 	public static void main(String[] args) {
+	System.out.println("Select option");
+	System.out.println("1. Create");
+	System.out.println("2. Read");
+	System.out.println("3. Update");
+	System.out.println("4. Delete");
+	
+	Scanner sc = new Scanner(System.in);
+	int x = sc.nextInt();
+	
+	switch(x) {
+	case 1:
+		CreatePatient.main(null);
 		
+	case 2:
+		ReadPatient.main(null);
+	
+	case 3:
+		UpdatePatient.main(null);
+	
+		
+	case 4:
+		DeletePatient.main(null);
+	}
+	
+	
 	}
 }
