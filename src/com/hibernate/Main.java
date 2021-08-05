0920d73a3b56ec1fa10f88c1bd5a35765a478b71
Lakshay Gupta <lakshay.gupta@timesinternet.in>
2021-08-05 12:40:02 +0530

    Added create operation

--- src/com/hibernate/Main.java
+++ src/com/hibernate/Main.java
@@ -5,23 +5,6 @@ import org.hibernate.SessionFactory;
 
 public class Main {
 	public static void main(String[] args) {
-		Doctor doctor = new Doctor();
-		doctor.setName("Lakshay");
-		Patient patient1 = new Patient();
-		patient1.setName("ABC1");
-		patient1.setDoctorDetails(doctor);
 		
-		Patient patient2 = new Patient();
-		patient2.setName("AAA2");
-		patient2.setDoctorDetails(doctor);
-		(doctor.getPatients()).add(patient1);
-		(doctor.getPatients()).add(patient2);
-		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-		Session session = sessionFactory.openSession();
-		session.beginTransaction();
-		session.save(doctor);
-		session.getTransaction().commit();
-		session.close();
-		sessionFactory.close();
 	}
 }
