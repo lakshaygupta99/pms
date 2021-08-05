6418b572df682d93a14b0d244582a52cf04791fe
Lakshay Gupta <lakshay.gupta@timesinternet.in>
2021-08-05 12:42:40 +0530

    Added delete operation

---
+++ src/com/hibernate/DeletePatient.java
@@ -0,0 +1,22 @@
+package com.hibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+public class DeletePatient {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+		// getting session object from session factory
+		Session session = sessionFactory.openSession();
+		// getting transaction object from session object
+		session.beginTransaction();
+		Patient student = (Patient) session.load(Patient.class, 2);// delete where id=2
+		session.delete(student);
+		System.out.println("Deleted Successfully");
+		session.getTransaction().commit();
+		sessionFactory.close();
+	}
+
+}
